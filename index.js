bafae1fd297e7970bdb0220baf65ace205b42ddd
Scarecrrow <tygima@gmail.com>
2020-05-26 18:36:58 -0700

    initial

---
+++ index.js
@@ -0,0 +1,107 @@
+(function () {
+	let vm = this;
+	let bestLocalCafe_lat = 33.62283;
+	let bestLocalCafe_long = -117.922305;
+	let gmap, gmapTarget, gmapOptions;
+	let bmap, bmapTarget, bmapOptions;
+
+	// === VARIABLE HOIST ===
+	vm.showGmap = true;
+
+	// === FUNCTION HOIST ===
+	vm.initGmap = _initGmap;
+	vm.initBmap = _initBmap;
+
+	// === LOCAL FUNCTIONS ===
+	function showMap(mapId) {
+		switch (mapId) {
+			case "gmapBtn":
+				gmapTarget.classList.remove("map-hidden");
+				bmapTarget.classList.add("map-hidden");
+
+				return;
+			case "bmapBtn":
+				bmapTarget.classList.remove("map-hidden");
+				gmapTarget.classList.add("map-hidden");
+
+				return;
+			default:
+				return;
+		}
+	}
+
+	// === HOISTED FUNCTIONS ===
+	function _initGmap() {
+		gmapOptions = {
+			zoom: 11,
+			center: {
+				lat: bestLocalCafe_lat,
+				lng: bestLocalCafe_long,
+			},
+		};
+
+		gmap = new google.maps.Map(gmapTarget, gmapOptions);
+
+		const infowindow = new google.maps.InfoWindow({
+			content: `
+        <div id="content">
+          <div id="siteNotice"></div>
+          <h4 id="firstHeading" class="firstHeading">Mariner's Coffee Shop</h1>
+          <div id="bodyContent">
+            <p>Best small breakfast cafe around!</p>
+          </div>
+        </div>
+      `,
+		});
+
+		const marker = new google.maps.Marker({
+			position: gmapOptions.center,
+			map: gmap,
+			title: "Mariner's Coffee Shop",
+		});
+
+		marker.addListener("click", function () {
+			infowindow.open(gmap, marker);
+		});
+	}
+
+	function _initBmap() {
+		bmapOptions = {
+			zoom: 11,
+			center: new Microsoft.Maps.Location(
+				bestLocalCafe_lat,
+				bestLocalCafe_long
+			),
+			mapTypeId: Microsoft.Maps.MapTypeId.road,
+		};
+
+		bmap = new Microsoft.Maps.Map(bmapTarget, bmapOptions);
+
+		const pushpin = new Microsoft.Maps.Pushpin(bmapOptions.center, {
+			text: "M",
+			title: "Mariner's Coffee Shop",
+			subTitle: "Best Locals Cafe",
+			color: "#ee5253",
+		});
+
+		bmap.entities.push(pushpin);
+	}
+
+	function initialize() {
+		gmapTarget = document.getElementById("gmap");
+		bmapTarget = document.getElementById("bmap");
+
+		// Register btn click handler
+		document.addEventListener("click", (event) => {
+			const target = event.target.closest("btn");
+
+			if (event.target) {
+				event.preventDefault();
+				showMap(event.target.id);
+			}
+		});
+	}
+
+	// === INIT ===
+	initialize();
+})();
